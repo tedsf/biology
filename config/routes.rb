45533565dec2f4ee56dc5a826613a8d01ff42339
Mikey Moore <email": null>
2016-07-15 13:04:06 -0700

    Remove rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :products, :users, :sessions, :categories
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  resources :products, :users, :sessions, :categories, :admin
 
   root "products#index"
 end
