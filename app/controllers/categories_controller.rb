28575b4ff13b098781c4d1effe02531c1b6dd3bb
Mikey Moore <email": null>
2016-07-15 12:26:01 -0700

    Fix views for categories show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,17 @@
 class CategoriesController < ApplicationController
   def show
-    product_query = Product.where(category_id: params[:id])
+    product_query = CategoriesProduct.where(category_id: params[:id])
 
     p product_query.many?
     if product_query.many?
-      @products = product_query
+      relationships = product_query
+      relationships.each do |relationship|
+        @products = []
+        @products.push(Product.find(relationship.product_id))
+      end
     else
-      @product = product_query[0]
+      relationship = product_query[0]
+      @product = Product.find(relationship.product_id)
     end
   end
 end
