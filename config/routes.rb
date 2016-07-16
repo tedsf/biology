802927376c4618c20458079387fc89baf7cef0b2
Christel Trutmann <ctrutmann@gmail.com>
2016-07-15 20:06:35 -0700

    Restrict sessions routes to new, create, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-  resources :sessions
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :orders
 
