d09864d111a6ab07daa23b5bddb23c8447bd43e8
Christel Trutmann <ctrutmann@gmail.com>
2016-07-15 19:47:49 -0700

    Fix RailsAdmin authorization bug

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,23 +1,10 @@
 RailsAdmin.config do |config|
-  # config.authenticate_with do
-  #   authenticate_or_request_with_http_basic('Site Message') do |username, password|
-  #     username == 'admin' && password == 'secret'
-  #   end
-  # end
+  include ApplicationHelper
 
-  # config.current_user do
-  #   user = User.find(session[:user_id])
-  #   if user
-  #     user
-  #   else
-  #     flash[:danger] = "You need to login to access"
-  #     redirect_to root_path
-  #   end
-  # end
-
-  # config.authorize_with do
-  #   redirect_to root_path unless current_user.try(:admin?)
-  # end
+  # FUNKY: Try to change '/products' back to root_path
+  config.authorize_with do
+    redirect_to '/products' unless current_user.try(:admin?)
+  end
 
   config.actions do
     dashboard                     # mandatory
