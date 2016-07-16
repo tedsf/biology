1d46c04ecf0e8beef5dfd0da48a5950a16827cb9
Christel Trutmann <ctrutmann@gmail.com>
2016-07-15 21:12:48 -0700

    Nest orders inside users routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
-  resources :orders
+  resources :users, only: [:new, :create] do
+    resources :orders
+  end
 
   root "products#index"
 end
