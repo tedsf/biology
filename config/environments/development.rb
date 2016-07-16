5af4b9a7a017a6735f59e0d5da55b388f8f5f861
Mikael <miketekle14@gmail.com>
2016-07-16 12:25:24 -0700

    add 'cart', and 'add to cart' buttons to show page and layout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,9 +47,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
+  :domain               => "gmail.com",
   :user_name            => ENV['GMAIL_USERNAME'],
   :password             => ENV['GMAIL_PASSWORD'],
-  :authentication       => "plain",
+  :authentication       => "login",
   :enable_starttls_auto => true
   }
 
