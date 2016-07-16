bb6cfc1d100bc66628ee5f2ad929f9469995168b
Christel Trutmann <ctrutmann@gmail.com>
2016-07-16 15:46:38 -0700

    Add quantity_in_stock validation to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,16 @@ class Order < ActiveRecord::Base
 
   before_create :order_price
 
+  validate :quantity_in_stock
 
-  # Validation for order quantity not exceeding product quantity.
-  # Callback for calculating order_price
   def order_price
     self.order_price = (self.product.price * self.quantity * 100).round / 100.0
+  end
+
+  private
+  def quantity_in_stock
+    if self.quantity > self.product.quantity
+      errors.add(:quantity, "can't be greater than the quantity in stock")
+    end
   end
 end
